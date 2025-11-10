d3c13e83752e8eaab12de574b9278c40574c1e2b
Duc Viet <vietdao464@gmail.com>
2025-11-10 15:50:25 +0700

    add project

--- WebBanLapTop/Areas/Admin/Controllers/ProductsController.cs
+++ WebBanLapTop/Areas/Admin/Controllers/ProductsController.cs
@@ -14,25 +14,34 @@ namespace WebBanLapTop.Areas.Admin.Controllers
         // GET: Admin/Products
         DatabaseDataContext db = new DatabaseDataContext();
 
-        public ActionResult Index(int? page)
-        {
+         public ActionResult Index(int? page, string search)
+         {
             int pageSize = 5;
             int pageNumber = page ?? 1;
-
-
-            var paged = db.vw_ProductWithDiscountCounts
-                          .OrderByDescending(x => x.product_id)
-                          .ToPagedList(pageNumber, pageSize);
-
-
+            var query = db.vw_ProductWithDiscountCounts
+                          .Where(x => x.is_delete == false || x.is_delete == null);
+            //tim kiem
+            if (!string.IsNullOrEmpty(search))
+            {
+                search = search.Trim().ToLower();
+                query = query.Where(x => x.name.ToLower().Contains(search));
+            }
+            var disProducts = query.GroupBy(x => x.product_id).Select(g => g.FirstOrDefault());
+            var paged = disProducts.OrderByDescending(x => x.product_id)
+                            .ToPagedList(pageNumber, pageSize);
             ViewBag.PageSize = pageSize;
             ViewBag.Page = pageNumber;
+            ViewBag.Search = search;
 
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_ProductListPartial", paged);
+            }
             return View(paged);
         }
         public JsonResult getProductName()
         {
-            var data = db.tb_products.Select(x => new
+            var data = db.tb_products.Where(x => x.is_delete == false || x.is_delete == null).Select(x => new
             {
                 x.product_id,
                 x.name
@@ -107,14 +116,15 @@ namespace WebBanLapTop.Areas.Admin.Controllers
                     }
                     else
                     {
-                
+
                         model.image = images[0];
                     }
                 }
 
                 model.create_at = DateTime.Now;
+                model.is_delete = false;
                 db.tb_products.InsertOnSubmit(model);
-                db.SubmitChanges(); 
+                db.SubmitChanges();
                 if (images != null && images.Count > 0)
                 {
                     foreach (var img in images)
@@ -136,5 +146,98 @@ namespace WebBanLapTop.Areas.Admin.Controllers
                 return Json(new { success = false, message = "Lỗi: " + ex.Message + (ex.InnerException != null ? " | " + ex.InnerException.Message : "") });
             }
         }
+        public ActionResult Edit() { 
+
+            return View();
+        }
+        [HttpPost]
+        public JsonResult LayTTSP(int id)
+        {
+            try
+            {
+                var sp = db.tb_products.SingleOrDefault(x => x.product_id == id);
+                if (sp != null)
+                {
+                    var spdata = new
+                    {
+                        product_id = sp.product_id,
+                        name = sp.name,
+                        category_id = sp.category_id,
+                        brand_id = sp.brand_id,
+                        price = sp.price,
+                        description = sp.description,
+                        quantity = sp.quantity,
+                        image = sp.image,
+                        create_at = sp.create_at,
+                        is_delete = sp.is_delete
+                    };
+                    return Json(new { success = true, data = spdata }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Không tìm thấy sản phẩm." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Lỗi: " + ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public JsonResult Update(tb_product model)
+        {
+            try
+            {
+                int id = int.Parse(Request["product_id"]);
+                var sp = db.tb_products.FirstOrDefault(x => x.product_id == id);
+                if (sp != null)
+                {
+                    sp.name = model.name;
+                    sp.category_id = model.category_id;
+                    sp.brand_id = model.brand_id;
+                    sp.price = model.price;
+                    sp.description = model.description;
+                    sp.quantity = model.quantity;
+                    db.SubmitChanges();
+                    return Json(new { success = true, message = "Cập nhật sản phẩm thành công!" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Không tìm thấy sản phẩm." }, JsonRequestBehavior.AllowGet);
+                }
+               
+            }
+            catch(Exception ex)
+            {
+                return Json(new { success = false, message = "Lỗi: " + ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            try
+            {
+                var sp = db.tb_products.FirstOrDefault(x => x.product_id == id);
+                var discount = db.tb_product_discounts.Where(x => x.product_id == id);
+                var images = db.tb_product_images.Where(x => x.product_id == id).ToList();
+                if (sp != null)
+                {
+                    sp.is_delete = true;
+                    db.tb_product_discounts.DeleteAllOnSubmit(discount);
+                    db.tb_product_images.DeleteAllOnSubmit(images);
+                    db.SubmitChanges();
+                    return Json(new { success = true, message = "Xóa sản phẩm thành công!" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Không tìm thấy sản phẩm." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Lỗi: " + ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
     }
 }
